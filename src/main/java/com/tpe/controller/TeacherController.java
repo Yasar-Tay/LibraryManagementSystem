75df6fcd50377736ec11734a7d928a56867d26d3
Yasar TAY <yasartay@gmail.com>
2023-08-07 00:42:00 +0300

    Teacher CRUD operations 1..

--- src/main/java/com/tpe/controller/TeacherController.java
+++ src/main/java/com/tpe/controller/TeacherController.java
@@ -1,7 +1,38 @@
 package com.tpe.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.tpe.domain.Teacher;
+import com.tpe.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/teachers") //http;//localhost:8081/teachers
 public class TeacherController {
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> saveTeacher(@Valid @RequestBody Teacher teacher){
+        teacherService.saveTeacher(teacher);
+        Map<String,String> responseMap = new HashMap<>();
+        responseMap.put("message", "Teacher is saved successfully");
+        responseMap.put("status", "True");
+
+        return new ResponseEntity<>(responseMap, HttpStatus.CREATED); //201
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Teacher>> listAllTeachers(){
+        List<Teacher> teacherList = teacherService.findAllTeachers();
+        return new ResponseEntity<>(teacherList, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/tpe/service/TeacherService.java
+++ src/main/java/com/tpe/service/TeacherService.java
@@ -1,8 +1,13 @@
 package com.tpe.service;
 
+import com.tpe.domain.Teacher;
+import com.tpe.exception.ConflictException;
+import com.tpe.exception.ResourceNotFoundException;
 import com.tpe.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TeacherService {
@@ -11,4 +16,22 @@ public class TeacherService {
     private TeacherRepository teacherRepository;
 
 
+    public void saveTeacher(Teacher teacher) {
+        Teacher existingTeacher = teacherRepository.findByEmail(teacher.getEmail());
+
+        if (existingTeacher != null){
+            throw new ConflictException(String.format("Teacher with email: %s already exists!", teacher.getEmail()));
+        }
+
+        teacherRepository.save(teacher);
+    }
+
+    public List<Teacher> findAllTeachers() {
+        List<Teacher> teacherList = teacherRepository.findAll();
+
+        if (teacherList.isEmpty())
+            throw new ResourceNotFoundException("No teacher...");
+
+        return teacherList;
+    }
 }
